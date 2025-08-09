0182601c6121b6524f7702874c4e14d708899f10
Walter Martinez Perales <161175383+WalterMP1@users.noreply.github.com>
2025-08-08 22:21:15 -0600

    Update 01_esquema_base.sql

--- 01_esquema_base.sql
+++ 01_esquema_base.sql
@@ -6,30 +6,30 @@ USE tienda_celulares;
 CREATE TABLE clientes
 (
 	id_cliente INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100),
-    telefono VARCHAR(20)
+    nombre VARCHAR(100) NOT NULL,
+    telefono VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE proveedores
 (
 	id_proveedor INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100),
-    telefono VARCHAR(20)
+    nombre VARCHAR(100) NOT NULL,
+    telefono VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE marcas
 (
 	id_marca INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100)
+    nombre VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE productos
 (
 	id_producto INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100),
-    id_marca INT,
-    precio_unitario DECIMAL(10,2),
-    id_proveedor INT,
+    nombre VARCHAR(100) NOT NULL,
+    id_marca INT NOT NULL,
+    precio_unitario DECIMAL(10,2) NOT NULL,
+    id_proveedor INT NOT NULL,
     FOREIGN KEY (id_marca) REFERENCES marcas(id_marca),
     FOREIGN KEY (id_proveedor) REFERENCES proveedores(id_proveedor)
 );
@@ -37,18 +37,18 @@ CREATE TABLE productos
 CREATE TABLE ventas
 (
 	id_venta INT AUTO_INCREMENT PRIMARY KEY,
-    id_cliente INT,
-    fecha DATE,
+    id_cliente INT NOT NULL,
+    fecha DATE NOT NULL,
     FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
 );
 
 CREATE TABLE detalle_venta
 (
 	id_detalle INT AUTO_INCREMENT PRIMARY KEY,
-    id_venta INT,
-    id_producto INT,
-    cantidad INT,
-    precio_venta_unitario DECIMAL(10,2),
+    id_venta INT NOT NULL,
+    id_producto INT NOT NULL,
+    cantidad INT NOT NULL,
+    precio_venta_unitario DECIMAL(10,2) NOT NULL,
     FOREIGN KEY (id_venta) REFERENCES ventas(id_venta),
     FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
 );
